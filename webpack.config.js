426829b1fbfe02dc0293b88b1d7de2206e98a316
zfoam91 <azelmaava@gmail.com>
2024-09-23 19:38:29 +1000

    Sunny Cafe

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+//const path = require('path');
+
+//module.exports = {
+//    entry: 'script.js', // Adjust this to your main JS file
+//    output: {
+//        filename: 'bundle.js',
+//        path: path.resolve(__dirname, 'dist'), // Where to output the bundle
+//    },
+//    mode: 'production', // or 'development'
+//};
